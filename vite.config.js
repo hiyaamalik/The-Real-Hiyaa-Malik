f90112a9b78023037667ea56068ae674a96b3000
Hiyaa Malik <hiyaamalik205@gmail.com>
2025-05-23 21:24:58 +0530

    Fixes in config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'build', // Ensure the build output is set to 'build' folder
-  },
-  base: '/My-Portfolio/', 
-});
+    outDir: 'dist'  // Change this line to 'dist'
+  }
+})
